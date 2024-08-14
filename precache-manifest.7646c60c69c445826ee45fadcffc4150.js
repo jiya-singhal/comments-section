2ac78554c137ce70d168cfde5f94ae086bf12b42
Jiya Singhal <jiyasinghal95@gmail.com>
2024-08-14 18:44:28 +0530

    Updates

---
+++ precache-manifest.7646c60c69c445826ee45fadcffc4150.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62ae543721e2ef30faae913fd0f7a0cc",
+    "url": "/comments-section/index.html"
+  },
+  {
+    "revision": "0df0bd1c12440f4f6aac",
+    "url": "/comments-section/static/css/main.b75fb5bd.chunk.css"
+  },
+  {
+    "revision": "5da267c82baf823c898d",
+    "url": "/comments-section/static/js/2.6810a231.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/comments-section/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "0df0bd1c12440f4f6aac",
+    "url": "/comments-section/static/js/main.042c5e58.chunk.js"
+  },
+  {
+    "revision": "c0b7245391eb77b89608",
+    "url": "/comments-section/static/js/runtime~main.c2ca727f.js"
+  }
+]);
